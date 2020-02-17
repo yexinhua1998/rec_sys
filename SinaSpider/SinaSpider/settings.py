d047ee7d30e9332881a68de6ed9fc42d84fca15f
Sinward Ye <244649399@qq.com>
2020-02-17 13:58:24 +0800

    更改了逻辑，增加了对index和普通页面的区分

--- SinaSpider/SinaSpider/settings.py
+++ SinaSpider/SinaSpider/settings.py
@@ -38,7 +38,7 @@ CONCURRENT_REQUESTS = 5000
 #DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 CONCURRENT_REQUESTS_PER_DOMAIN = 1000
-#CONCURRENT_REQUESTS_PER_IP = 16
+CONCURRENT_REQUESTS_PER_IP = 200
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
--- SinaSpider/SinaSpider/spiders/SinaSpider.py
+++ SinaSpider/SinaSpider/spiders/SinaSpider.py
@@ -19,24 +19,41 @@ class SinaSpider(scrapy.Spider):
 	def parse(self,response):
 		#返回当前页面的内容
 		if isinstance(response,scrapy.http.TextResponse):
-			print('getted page:%s'%response.url)
-			yield SinaPageItem(url=response.url,content=response.text)
+			link_matcher=re.compile('^https?://(.*)$')#提取地址，忽略协议
+			sina_matcher=re.compile('^(.*)sina.com.cn(.*)$')#判断是否新浪网的链接
+			index_matcher=re.compile('^(.*)sina.com.cn(/[^/]+/)?$')#判断是否是索引页的页面
 
-			matcher=re.compile('http://(.*)sina.com.cn(.*)')
+
+			link=link_matcher.match(response.url).group(1)
+			if index_matcher.match(link) is None:
+				#不是索引页面，应该存储
+				yield SinaPageItem(url=link,content=response.text)
+
 			cursor=self.conn.cursor()
 
-			linklist=response.xpath('//a/@href').getall()
-			#np.random.shuffle(linklist)#随机排列
-			for link in linklist:
-				if matcher.match(link) is None:
+			hreflist=response.xpath('//a/@href').getall()
+			for href in hreflist:
+				#去掉协议头
+				match=link_matcher.match(href)
+				if match is None:
+					continue
+				link=match.group(1)
+
+				#根据地址类型进行处理
+				if sina_matcher.match(link) is None:
 					#不是新浪网的链接
 					continue
-				cursor.execute('SELECT is_url_getted($url$%s$url$);'%link)
-				data=cursor.fetchall()
-				if data[0][0]==True:
-					#数据库已经有该页面的数据
-					continue
-				yield scrapy.Request(url=link,callback=self.parse)
+				if not index_matcher.match(link) is None:
+					#是索引页，直接返回
+					yield scrapy.Request(url='http://'+link,callback=self.parse)
+				else:
+					#不是索引页，根据是否已经爬取进行返回
+					cursor.execute('SELECT is_url_getted($url$%s$url$);'%link)
+					data=cursor.fetchall()
+					if data[0][0]==True:
+						#数据库已经有该页面的数据
+						continue
+					yield scrapy.Request(url='http://'+link,callback=self.parse)
 			cursor.close()
 		else:
 			return []
