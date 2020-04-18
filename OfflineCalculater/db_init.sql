75178b722734760da30b68916aa690e7a785b8df
Sinward Ye <244649399@qq.com>
2020-04-18 09:56:03 +0800

    增加了most sim和index

--- OfflineCalculater/db_init.sql
+++ OfflineCalculater/db_init.sql
@@ -1,4 +1,17 @@
 CREATE TABLE doc_vec(
 	url TEXT,
-	vec double precision[300]
+	vec double precision[300],
+	docid Integer
 );
+
+
+CREATE INDEX docVec_docid_index ON doc_vec(docid);
+CREATE INDEX docVec_url_index ON doc_vec using HASH(url);
+
+CREATE TABLE most_sim(
+	docid1 Integer,
+	docid2 Integer,
+	sim double precision
+);
+
+CREATE INDEX most_sim_docid1_index ON most_sim using HASH(docid1);
